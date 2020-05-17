27b214f682f3cd1a68a693d244e16e6236488404
Akshay Jain <akshay1427@gmail.com>
2020-05-17 09:59:55 +0100

    Initial commit

---
+++ ToDo/Controllers/DetailViewController.swift
@@ -0,0 +1,73 @@
+//
+//  DetailViewController.swift
+//  ToDo
+//
+//  Created by Akshay Jain on 14/05/2020.
+//  Copyright © 2020 Akshay Jain. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+    @IBOutlet weak var mapView: MKMapView!
+    var itemInfo: (ItemManager, Int)?
+    
+    let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        return dateFormatter
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        guard let itemInfo = itemInfo else { return }
+        let item = itemInfo.0.itemAtIndex(itemInfo.1)
+        
+        titleLabel.text = item.title
+        locationLabel.text = item.location?.name
+        descriptionLabel.text = item.itemDescription
+        
+        if let timestamp = item.timeStamp {
+            let date = Date(timeIntervalSince1970: timestamp)
+            dateLabel.text = dateFormatter.string(from: date)
+        }
+        
+        if let coordinate = item.location?.coordinate {
+            let region = MKCoordinateRegion(center: coordinate,
+                                            latitudinalMeters: 100, longitudinalMeters: 100)
+            mapView.region = region
+        }
+    }
+    
+    func checkItem() {
+        if let itemInfo = itemInfo {
+            itemInfo.0.checkItemAtIndex(itemInfo.1)
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
